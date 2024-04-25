5b63c217eaa66edc5ced19b85825a7464350c22b
Koz <kenneth.averna@gmail.com>
2024-04-25 10:19:16 -0700

    Initial Commit

---
+++ Limpio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Limpio
+//
+//  Created by Kenny Averna on 4/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Limpio/LimpioApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimpioApp.swift
+//  Limpio
+//
+//  Created by Kenny Averna on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LimpioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
